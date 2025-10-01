a7f67a31f1899a970b8a62ddd64770ec4bdbbe13
gvbvdxxalt3 <jasonglenevans2010@outlook.com>
2025-10-01 13:51:54 -0400

    Create custom-manager-script.js

---
+++ src/custom-manager-script.js
@@ -0,0 +1,9 @@
+//You can add a custom maanger to manage cloud variables and events here,
+//this works like the extension.
+//See ./ws-server.js for usage.
+function startCustomManager (cs,wss) {
+  //Write code here.
+  
+}
+
+module.exports = startCustomManager;
